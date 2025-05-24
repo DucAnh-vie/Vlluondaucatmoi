3804e3a8b729c486d854b1ddb9d5d933245e7f1b
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-05-24 17:31:29 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,37 +1,30 @@
 import argparse
-from ultralytics_custom.models.yolo.detect import DetectionTrainer, DetectionValidator
+from ultralytics_custom.models.yolo.detect import DetectionTrainer
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train và Validate YOLO model")
-    parser.add_argument("-m", "--model", type=str, default="yolov8n.pt",
-                        help="Weights/config của model")
+    parser.add_argument("-m", "--model", type=str, required=True,
+                        help="Đường dẫn tới file model.yaml TÙY CHỈNH")
     parser.add_argument("-d", "--data",  type=str, required=True,
                         help="Đường dẫn tới file dataset.yaml")
     parser.add_argument("-e", "--epochs", type=int, default=50,
                         help="Số epoch khi train")
     parser.add_argument("-i", "--imgsz", type=int, default=640,
                         help="Kích thước ảnh")
-    parser.add_argument("-p", "--pretrained", type=bool, default="True",
-                        help="Model đã được huấn luyện")
     return parser.parse_args()
 
 def main():
     args = parse_args()
 
     overrides = {
-        "model": args.model,
+        "model": args.model,        # ← file .yaml tùy chỉnh
         "data": args.data,
         "epochs": args.epochs,
         "imgsz": args.imgsz
     }
 
-    # Train (cfg dùng mặc định, overrides truyền config)
     trainer = DetectionTrainer(overrides=overrides)
     trainer.train()
-
-    # Validate (cũng qua overrides)
-    validator = DetectionValidator(overrides=overrides)
-    validator.valid()
 
 if __name__ == "__main__":
     main()
